40eda2b7f9fe43c5ab206515e3d8a6fa03b9e8a0
Mahesh <163907986+Mahesh5862@users.noreply.github.com>
2026-02-23 15:02:02 +0530

    Update reverse_trial_matcher.py

--- ClinRAG/src/reverse_trial_matcher.py
+++ ClinRAG/src/reverse_trial_matcher.py
@@ -3,15 +3,22 @@ from src.data_loader import load_patient_data
 from src.trial_matcher import match_trials
 
 
-def find_eligible_patients_for_trial(trial, patients_folder="data/ehr_samples"):
+def find_eligible_patients_for_trial(trial):
+
+    # 🔥 Get absolute project root
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    patients_folder = os.path.join(BASE_DIR, "data", "ehr_samples")
+
+    # 🔥 Safety check
+    if not os.path.exists(patients_folder):
+        return []
 
     eligible_patients = []
 
     for file in os.listdir(patients_folder):
-
         patient_path = os.path.join(patients_folder, file)
-        patient = load_patient_data(patient_path)
 
+        patient = load_patient_data(patient_path)
         matches = match_trials(patient, [trial])
 
         if matches:
